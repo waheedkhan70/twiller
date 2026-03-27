3d4da0f55de09e92994d10c7beb1299929ebc1e0
waheedkhan70 <waheed066khan@gmail.com>
2026-03-27 08:42:35 +0530

    update eslint error

--- twiller/next.config.ts
+++ twiller/next.config.ts
@@ -1,3 +1,19 @@
+// import type { NextConfig } from "next";
+
+// const nextConfig: NextConfig = {
+//   /* config options here */
+//   env: {
+//     BACKEND_URL: process.env.BACKEND_URL,
+//   },
+//   eslint: {
+//     ignoreDuringBuilds: true,
+//   },
+// };
+
+// export default nextConfig;
+
+
+
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
@@ -5,9 +21,7 @@ const nextConfig: NextConfig = {
   env: {
     BACKEND_URL: process.env.BACKEND_URL,
   },
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
+  // The 'eslint' block was removed from here
 };
 
 export default nextConfig;
